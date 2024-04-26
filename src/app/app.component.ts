6a8c7a901e6e990390a184081cfcde281d73de53
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-26 17:22:37 -0500

    chore(movies): change module config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TheToolbarContentComponent} from "./public/the-toolbar-content/the-toolbar-content.component";
 import {CardsMoviesComponent} from "./movies/components/cards-movies/cards-movies.component";
-import {TheHomePageComponent} from "./movies/pages/the-home-page/the-home-page.component";
+import {TheHomePageComponent} from "./movies/components/pages/the-home-page/the-home-page.component";
 
 @Component({
   selector: 'app-root',
