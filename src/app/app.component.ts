b4fe6e71e27c99be78c5857b946b3f2f0477c49c
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-26 17:44:33 -0500

    feat(movies/pages): added the home page body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TheToolbarContentComponent} from "./public/the-toolbar-content/the-toolbar-content.component";
 import {CardsMoviesComponent} from "./movies/components/cards-movies/cards-movies.component";
-import {TheHomePageComponent} from "./movies/pages/the-home-page/the-home-page.component";
+import {TheHomePageComponent} from "./movies/pages/the-home-pages/the-home-pages.component";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/movies/pages/the-home-page/the-home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './the-home-page.component.html',
+  styleUrl: './the-home-page.component.css'
+})
+export class TheHomePageComponent {
+
+}
