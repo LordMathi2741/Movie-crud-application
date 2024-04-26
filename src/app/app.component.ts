cf0905ae7e304ba4dcedf42f6820b1aac4206fa2
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-26 17:54:35 -0500

    feat: added table content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TheToolbarContentComponent} from "./public/the-toolbar-content/the-toolbar-content.component";
 import {CardsMoviesComponent} from "./movies/components/cards-movies/cards-movies.component";
-import {TheHomePageComponent} from "./movies/pages/the-home-pages/the-home-pages.component";
+import {TheHomePageComponent} from "./movies/pages/the-home-page/the-home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TheToolbarContentComponent, CardsMoviesComponent, TheHomePageComponent],
+  imports: [RouterOutlet, TheToolbarContentComponent, CardsMoviesComponent, TheHomePageComponent, TheHomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/movies/pages/the-home-page/the-home-page.component.ts
+++ src/app/movies/pages/the-home-page/the-home-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import { Movie } from "../../model/movie.entity";
+import {MovieService} from "../../service/movie-service.service";
 
 @Component({
-  selector: 'app-the-home-page',
+  selector: 'the-home-page',
   standalone: true,
-  imports: [],
+  imports: [ MatTableModule],
   templateUrl: './the-home-page.component.html',
   styleUrl: './the-home-page.component.css'
 })
-export class TheHomePageComponent {
+export class TheHomePageComponent implements OnInit {
+  dataSource = new MatTableDataSource();
+  movies: Movie[] = [];
+  movieData!: Movie;
+  displayedColumns = ['id', 'title', 'genre', 'duration', 'imgUrl'];
+
+  @ViewChild(MatSort, {static:true}) sort!: MatSort;
+  constructor(private movieService: MovieService) {
+     this.movieData = {} as Movie;
+  }
+  ngOnInit(): void {
+    this.getMovies();
+  }
+  getMovies(){
+    this.movieService.getAll().subscribe((data: any) => {
+      data.forEach((element: any) => {
+        this.movieData = new Movie(element.id, element.title, element.genre, element.duration, element.imgUrl);
+        this.movies.push(this.movieData);
+      });
+      this.dataSource.data = this.movies;
+    });
+  }
 
 }
